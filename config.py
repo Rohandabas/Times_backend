8428654d71c8366146be8b85a15250403cbac427
Rohandabas <rohandabasbij004@gmail.com>
2025-07-04 14:36:42 +0530

    added

--- config.py
+++ config.py
@@ -1,23 +1,18 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
-    REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
-    REDDIT_USER_AGENT = os.getenv('REDDIT_USER_AGENT')
-    FLASK_PORT = int(os.getenv('FLASK_PORT', 5000))
-    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+    REDDIT_CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
+    REDDIT_CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
+    REDDIT_USER_AGENT = os.environ['REDDIT_USER_AGENT']
+    FLASK_PORT = int(os.environ.get('FLASK_PORT', 5000))
+    GEMINI_API_KEY = os.environ['GEMINI_API_KEY']
     
-    # Subreddits to monitor
     MONITORED_SUBREDDITS = [
         'worldnews', 'technology', 'science', 'business', 'politics',
         'news', 'economics', 'environment', 'health', 'education',
         'space', 'artificial', 'programming', 'startups', 'investing'
     ]
     
-    # News value keywords for scoring
     NEWS_KEYWORDS = {
         'breaking': 10,
         'urgent': 9,
